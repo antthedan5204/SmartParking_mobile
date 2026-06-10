204a5567a7a22f1b8fb5e683b60aa734e9afa91e
antthedan5204 <maithedan5204@gmail.com>
2026-06-10 14:09:08 +0700

    fix: add DarwinInitializationSettings for iOS local notifications

--- lib/core/services/notification_service.dart
+++ lib/core/services/notification_service.dart
@@ -21,9 +21,17 @@ class NotificationService {
     
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
+        
+    const DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+    );
 
     const InitializationSettings initializationSettings = InitializationSettings(
       android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
     );
 
     await _notificationsPlugin.initialize(
